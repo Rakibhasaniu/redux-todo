b199ad78ceb4dba5ac4d57863bdc1db975494953
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2024-01-14 21:28:18 +0600

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "./components/ui/button";
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      My todo
+      <Button>My Button</Button>
+    </h1>
+  )
+}
